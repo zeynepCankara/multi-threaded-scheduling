299f72a56adf81a31e6a079c984bc5abf061ebc9
zeynepCankara <zeynepcankara402@gmail.com>
2021-03-21 13:59:39 +0300

    add sjf test

--- schedule.c
+++ schedule.c
@@ -62,7 +62,7 @@ double getRandExpTime(int mean, int lowerLimit)
 
 // Test funtions
 
-void testReadyQueue(struct readyqueue *rq)
+void fcfsTest(struct readyqueue *rq)
 {
     pushBurst(rq, 1, 1, 3);
     pushBurst(rq, 2, 2, 4);
@@ -78,12 +78,33 @@ void testReadyQueue(struct readyqueue *rq)
     b = fcfs(rq);
     pushBurst(rq, 4, 6, 6);
     pushBurst(rq, 4, 7, 6);
-    printf("(4, 5, 6, 7) printed \n");
     printReadyqueue(rq->head);
+    printf("(4, 5, 6, 7) printed \n");
     deleteReadyqueue(rq->head);
 }
 
-void testRandomExpGenerator(int mean, int lowerLimit)
+void sjfTest(struct readyqueue *rq)
+{
+    pushBurst(rq, 1, 8, 8);
+    pushBurst(rq, 1, 7, 7);
+    pushBurst(rq, 1, 9, 9);
+    printReadyqueue(rq->head);
+    printf("(8, 7, 9) printed \n");
+    struct burst *b = sjf(rq);
+    pushBurst(rq, 1, 3, 3);
+    pushBurst(rq, 1, 5, 5);
+    printReadyqueue(rq->head);
+    printf("(8, 9, 3, 5) printed \n");
+    b = sjf(rq);
+    b = sjf(rq);
+    pushBurst(rq, 1, 2, 2);
+    pushBurst(rq, 1, 1, 1);
+    printReadyqueue(rq->head);
+    printf("(8, 9, 2, 1) printed \n");
+    deleteReadyqueue(rq->head);
+}
+
+void getRandExpTimeTest(int mean, int lowerLimit)
 {
     int CNT_RANDOM_NUM = 10;
     for (int i = 0; i < CNT_RANDOM_NUM; i++)
@@ -177,7 +198,7 @@ int main(int argc, char *argv[])
     }
 
     printf("main: all threads terminated\n");
-    testRandomExpGenerator(avgA, minA);
+    sjfTest(rq);
 
     return 0;
 }
